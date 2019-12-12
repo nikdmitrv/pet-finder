2e3ac0d0b3dbab3634c3c4f7041b87ebe51e5b4b
Alexander Kulikov <generatex@yandex.ru>
2019-12-12 17:17:36 +0300

    MVP fixed

--- frontend/src/components/LostForm/LostForm.js
+++ frontend/src/components/LostForm/LostForm.js
@@ -1,79 +1,112 @@
-import React, { Component } from 'react'
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+import { connect } from "react-redux";
 
-class LostAdd extends Component {
-    state ={
-        message: null
-    }
+import { createFoundAdvertAC } from "../../redux/actions";
 
+class LostForm extends Component {
+  state = {
+    message: null
+  };
 
-
-handleInput = (event) => {
-    const data = {}
-    const field = event.target.name
-    data[field] = event.target.value
+  handleInput = event => {
+    const data = {};
+    const field = event.target.name;
+    data[field] = event.target.value;
     this.setState(data);
-}
+  };
 
-    handleSubmit = (event) => {
-        event.preventDefault();
+  handleSubmit = event => {
+    event.preventDefault();
 
-        const { dogBreed, dogDescription, dogSex,
-            authorName, authorEmail, authorPhoneNumber, authorAddress } = this.state
+    const {
+      dogBreed,
+      dogDescription,
+      dogSex,
+      authorName,
+      authorEmail,
+      authorPhoneNumber,
+      authorAddress
+    } = this.state;
 
-        const advert = {
-            dogData: {
-                breed: dogBreed,
-                description: dogDescription,
-                sex: dogSex,
-            },
-            authorData: {
-                name: authorName,
-                email: authorEmail,
-                phoneNumber: authorPhoneNumber,
-                adress: authorAddress,
-            }
-        }
-    axios.post('api/lost', advert)
-    .then(res=>console.log("собака на месте ")
-    )
-    
-}
+    const advert = {
+      dogData: {
+        breed: dogBreed,
+        description: dogDescription,
+        sex: dogSex
+      },
+      authorData: {
+        name: authorName,
+        email: authorEmail,
+        phoneNumber: authorPhoneNumber,
+        adress: authorAddress
+      }
+    };
+    axios.post("api/lost", advert).then(res => console.log("собака на месте "));
+  };
 
-
-
-render() {
+  render() {
     return (
-        <div>
-               <form onSubmit={this.handleSubmit}>
-                <label for='dog-breed'>Порода:</label>
-                <input onChange={this.handleInput} name='dogBreed' id='dog-breed' type='text' />
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label for="dog-breed">Порода:</label>
+          <input
+            onChange={this.handleInput}
+            name="dogBreed"
+            id="dog-breed"
+            type="text"
+          />
 
-                <label for='dog-description'>Описание:</label>
-                <input onChange={this.handleInput} name='dogDescription' id='dog-description' type='text' />
+          <label for="dog-description">Описание:</label>
+          <input
+            onChange={this.handleInput}
+            name="dogDescription"
+            id="dog-description"
+            type="text"
+          />
 
-                <label for='dog-sex'>Пол:</label>
-                <input onChange={this.handleInput} name='dogSex' id='dog-sex' />
+          <label for="dog-sex">Пол:</label>
+          <input onChange={this.handleInput} name="dogSex" id="dog-sex" />
 
-                <label for='author-name'>Имя:</label>
-                <input onChange={this.handleInput} name='authorName' id='author-name' />
+          <label for="author-name">Имя:</label>
+          <input
+            onChange={this.handleInput}
+            name="authorName"
+            id="author-name"
+          />
 
-                <label for='author-email'>Email:</label>
-                <input onChange={this.handleInput} name='authorEmail' id='author-email' />
+          <label for="author-email">Email:</label>
+          <input
+            onChange={this.handleInput}
+            name="authorEmail"
+            id="author-email"
+          />
 
-                <label for='author-phoneNumber'>Телефонный номер:</label>
-                <input onChange={this.handleInput} name='authorPhoneNumber' id='author-phoneNumber' />
+          <label for="author-phoneNumber">Телефонный номер:</label>
+          <input
+            onChange={this.handleInput}
+            name="authorPhoneNumber"
+            id="author-phoneNumber"
+          />
 
-                <label for='author-address'>Адрес:</label>
-                <input onChange={this.handleInput} name='authorAddress' id='author-address' />
+          <label for="author-address">Адрес:</label>
+          <input
+            onChange={this.handleInput}
+            name="authorAddress"
+            id="author-address"
+          />
 
-                <button>Submit</button>
-            </form>
-            
-        </div>
-    )
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
-}
 
+const mapDispatchToProps = dispatch => {
+  return {
+    createFoundAdvert: advert => dispatch(createFoundAdvertAC(advert))
+  };
+};
 
-export default LostAdd;
+export default connect(null, mapDispatchToProps)(LostForm);
