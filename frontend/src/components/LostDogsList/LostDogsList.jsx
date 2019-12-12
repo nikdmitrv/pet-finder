fb23ee42e75750f1ab8d7f53f7d790a04bedee7e
Alexander Kulikov <generatex@yandex.ru>
2019-12-12 11:54:09 +0300

    fixed imports

--- frontend/src/components/LostDogsList/LostDogsList.jsx
+++ frontend/src/components/LostDogsList/LostDogsList.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { requestLostDogsAC } from "../../redux/actions";
 
 class LostDogsList extends Component {
   componentDidMount() {
