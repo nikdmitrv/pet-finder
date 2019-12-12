45589d868d40ee47c03a4c7d1f136ce467403636
Alexander Kulikov <generatex@yandex.ru>
2019-12-12 18:23:14 +0300

    redux-thunk done

--- backend/routes/adverts/found.js
+++ backend/routes/adverts/found.js
@@ -1,37 +1,33 @@
-const express = require('express');
+const express = require("express");
 
-const FoundDogAdvertModel = require('../../models/FoundDogAdvertModel')
-const { Animal, Author } = require('../../models/schemas/AdvertSchema')
+const FoundDogAdvertModel = require("../../models/FoundDogAdvertModel");
+const { Animal, Author } = require("../../models/schemas/AdvertSchema");
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.json(await FoundDogAdvertModel.getAll());
-})
+router.get("/", async (req, res) => {
+  res.json(await FoundDogAdvertModel.getAll());
+});
 
-router.post('/', async (req, res) => {
-    const { dogData, authorData } = req.body
-    const newAdvert = new FoundDogAdvertModel(
-        {
-            dogData: new Animal(
-                dogData.breed,
-                dogData.description,
-                dogData.sex
-            ),
-            authorData: new Author(
-                authorData.name,
-                authorData.email,
-                authorData.phoneNumber,
-                authorData.adress
-            ),
-            createdAt: Date.now()
-        })
-    newAdvert.save()
-        .then(() => {
-            res.json({ status: true, message: 'Saved!', newAdvert });
-        })
-        .catch((err) => {
-            res.json({ status: false, message: err })
-        })
-})
+router.post("/", async (req, res) => {
+  const { dogData, authorData } = req.body;
+  const newAdvert = new FoundDogAdvertModel({
+    dogData: new Animal(dogData.breed, dogData.description, dogData.sex),
+    authorData: new Author(
+      authorData.name,
+      authorData.email,
+      authorData.phoneNumber,
+      authorData.adress
+    ),
+    createdAt: Date.now()
+  });
+  newAdvert
+    .save()
+    .then(() => {
+      res.json({ message: "Объявление добавлено", newAdvert });
+    })
+    .catch(err => {
+      res.json({ message: err.message });
+    });
+});
 
 module.exports = router;
